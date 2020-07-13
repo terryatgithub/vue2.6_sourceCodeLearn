8d79063c043ff718acde54b19df27d792225e704
yuanbo <yuanbo@coocaa.com>
2020-07-13 22:03:30 +0800

    vue06 源码分析分析3

---
+++ vue06-sourcecode03/vue-study-web20-source/src/compiler/index.js
@@ -0,0 +1,29 @@
+/* @flow */
+
+import { parse } from './parser/index'
+import { optimize } from './optimizer'
+import { generate } from './codegen/index'
+import { createCompilerCreator } from './create-compiler'
+
+// `createCompilerCreator` allows creating compilers that use alternative
+// parser/optimizer/codegen, e.g the SSR optimizing compiler.
+// Here we just export a default compiler using the default parts.
+export const createCompiler = createCompilerCreator(function baseCompile (
+  template: string,
+  options: CompilerOptions
+): CompiledResult {
+  // 1.解析：将html字符串转换为AST
+  const ast = parse(template.trim(), options)
+
+  // 2.优化：标记静态节点
+  if (options.optimize !== false) {
+    optimize(ast, options)
+  }
+  // 3.生成：将ast转换为函数字符串
+  const code = generate(ast, options)
+  return {
+    ast,
+    render: code.render,
+    staticRenderFns: code.staticRenderFns
+  }
+})
